33db4ba6aa1aa461d796293dfc7cf7e48b7c5278
Pirawas Pirapongsilp <pirawas.p@ku.th>
2026-09-18 13:12:20 +0700

    try_except

---
+++ labs/week08-lab/example/try_except.py
@@ -0,0 +1,24 @@
+try:
+    Num1 = int(input("Enter a first Number: "))
+    Num2 = int(input("Enter a second Number: "))
+    signal = input("Enter signal: ")
+    result = 0
+
+    if signal == "+":
+        result = Num1 + Num2
+    elif signal == "-":
+        result = Num1 - Num2
+    elif signal == "*":
+        result = Num1 * Num2
+    elif signal == "/":
+        result = Num1 / Num2
+    else:
+        raise ValueError
+        print("Can Enter only signal + - * /")
+
+except ValueError:
+    print("Can enter only Number")
+except ZeroDivisionError:
+    print("Can not Division by 0")
+finally:
+    print("End")
